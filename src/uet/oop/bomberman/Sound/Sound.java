a383d835c8bded9789054247ebcaa9067ed6efaf
tramanh1511 <52901064+tramanh1511@users.noreply.github.com>
2022-10-04 15:39:32 +0700

    add mute sound

--- src/uet/oop/bomberman/Sound/Sound.java
+++ src/uet/oop/bomberman/Sound/Sound.java
@@ -1,43 +1,30 @@
 package uet.oop.bomberman.Sound;
 
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.*;
+import java.io.IOException;
 
 public class Sound {
-    public static boolean isPlaying = true;
-    public static void play(String name) {
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Clip clip = AudioSystem.getClip();
-                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                            getClass().getResourceAsStream("/music/" + name + ".wav"));
-                    clip.open(inputStream);
-                    clip.start();
-                    isPlaying = true;
-                } catch (Exception e) {
-                    System.err.println(e.getMessage());
-                }
-            }
-        }).start();
+    private Clip clip;
 
+    public Sound(String soundName) {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(
+                    getClass().getResourceAsStream("/music/" + soundName + ".wav"));
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
     }
-    public static void stop(String name){
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    System.out.println("00");
-                    Clip clip = AudioSystem.getClip();
-                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                            getClass().getResourceAsStream("/music/" + name + ".wav"));
-                    clip.open(inputStream);
-                    clip.stop();
-                    isPlaying = false;
-                } catch (Exception e) {
-                    System.err.println(e.getMessage());
-                }
+
+    public void play(Boolean playing) {
+        if (playing == false) {
+            if (clip.isRunning()) {
+                clip.stop();
             }
-        }).start();
+        } else {
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
     }
 }
